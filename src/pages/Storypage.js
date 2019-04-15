c3232fd3ff65f15fca325d65cd4f473d204e4448
vivian.zaddach@gmail.com <youwillnevergetthis10>
2019-04-15 13:32:31 +0200

    adapt styling to match page templates

--- src/pages/Storypage.js
+++ src/pages/Storypage.js
@@ -8,8 +8,12 @@ class Storypage extends Component {
     return (
       <div className="Storypage">
         <h1>Use Case 1</h1>
-        <img src={defaultImg} className="defaultImg" alt="logo" />
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        <div className="info-section">
+            <img src={defaultImg} className="defaultImg" alt="logo" />
+            <div className="story-text">
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+            </div>
+        </div>
         <button>go back</button>
         <button>go forward</button>
       </div>
