24bffa168ef2fd9d1e318d669ded097e5c6360e3
vivian.zaddach@gmail.com <youwillnevergetthis10>
2019-04-24 11:40:25 +0200

    show startpage by app start and save username in App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
 import Startpage from './pages/Startpage';
 import Storypage from './pages/Storypage';
-import Decisionpage from './pages/Decisionpage';
-import Jailpage from './pages/Jailpage';
 import './App.css';
 
 class App extends Component {
+
+constructor(props) {
+    super(props);
+    this.onSetNewPage = this.onSetNewPage.bind(this);
+    this.onSetUsername = this.onSetUsername.bind(this);
+    this.state = {
+    currentPage: <Startpage onSaveUsername={this.onSetUsername} />,
+    username: ''
+    };
+  }
+
+  onSetNewPage() {
+        this.setState({currentPage: <Storypage/>});
+  }
+
+  onSetUsername(name) {
+    this.setState({username: name});
+    alert('A name was submitted: ' + this.state.username);
+    this.onSetNewPage();
+  }
+
   render() {
     return (
-      <div className="App">
-        <Startpage/>
-        _______________________________________________________________________________________________________________
-        <Storypage/>
-         _______________________________________________________________________________________________________________
-        <Decisionpage/>
-         _______________________________________________________________________________________________________________
-         <Jailpage/>
+      <div className="Game">
+        {this.state.currentPage}
       </div>
     );
   }
-}
+
+  }
 
 export default App;
--- src/pages/Startpage.js
+++ src/pages/Startpage.js
@@ -3,16 +3,36 @@ import '../styles/style.css';
 
 
 class Startpage extends Component {
+
+ constructor(props) {
+    super(props);
+    this.state = {username: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({username: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.props.onSaveUsername(this.state.username);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="Startpage">
         <h1>Welcome</h1>
         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-        <label>
+        <form onSubmit={this.handleSubmit}>
+          <label>
             Enter your name:
-            <input type="text" name="name" />
+            <input type="text" value={this.state.username} onChange={this.handleChange} />
           </label>
-        <button>Let's go</button>
+          <input type="submit" value="Let's go" />
+        </form>
       </div>
     );
   }
