66e8833f9d9a49f77526ce6a5873bf9e8b1d8b3b
Veronika <veronika.babic@web.de>
2019-05-04 17:39:30 +0200

    made navbar size responsive

--- src/components/SideNavigation.js
+++ src/components/SideNavigation.js
@@ -6,14 +6,14 @@ import '../styles/style.css';
 class SideNavigation extends Component {
   burgerToggle() {
     var linksEl = document.querySelector('.narrowLinks');
-    //var sideNavigation = document.querySelector('.SideNavigation')
+    var sideNavigation = document.querySelector('.SideNavigation')
     if (linksEl.style.display === 'block') {
       linksEl.style.display = 'none';
-     // sideNavigation.style.width = '15%';
+      sideNavigation.style.width = '6%';
     } 
     else {
       linksEl.style.display = 'block';
-      //sideNavigation.style.width = '6%';
+      sideNavigation.style.width = '15%';
     }
 }
   
