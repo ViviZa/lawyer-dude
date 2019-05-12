b20e438df4d727cbb48f0d797f13322dbe60752f
Mario Schuetz <mayo2@gmx.de>
2019-05-12 17:55:19 +0200

    Replace buttons with Buttons component

--- src/pages/Startpage.jsx
+++ src/pages/Startpage.jsx
@@ -3,23 +3,21 @@ import SideNavigation from '../components/SideNavigation';
 import '../styles/style.css';
 import { withRouter } from 'react-router';
 import data from './Start.json';
+import Buttons from '../components/Buttons';
 
 class Startpage extends Component {
- constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       username: '',
-      textIndex: 0,
       panels: [],
       headline: ''
     };
-    this.nextText = this.nextText.bind(this);
-    this.previousText = this.previousText.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     const dataString = JSON.stringify(data);
     let jsonData = JSON.parse(dataString);
     console.log(jsonData);
@@ -29,13 +27,13 @@ class Startpage extends Component {
     })
   }
 
-  redirectToNextPage(){
-    const {history} = this.props;
+  redirectToNextPage() {
+    const { history } = this.props;
     history.push("/story");
   }
 
   handleChange(event) {
-    this.setState({username: event.target.value});
+    this.setState({ username: event.target.value });
   }
 
   handleSubmit(event) {
@@ -44,65 +42,24 @@ class Startpage extends Component {
     event.preventDefault();
   }
 
-  nextText() {
-    const {panels, textIndex} = this.state
-    const theSize = panels.length-1;
-    if(textIndex >= 0 && textIndex < theSize){
-        this.state.textIndex++;
-        this.setState({textIndex: this.state.textIndex});
-    }
-  }
-
-  previousText() {
-    const theSize = this.state.panels.length-1;
-    if(this.state.textIndex > 0 && this.state.textIndex <= theSize){
-        this.state.textIndex--;
-        this.setState({textIndex: this.state.textIndex});
-    }
-  }
-
   render() {
-    const {panels, textIndex, headline} = this.state;
+    const { panels, textIndex, headline } = this.state;
 
     return (
       <div className="Startpage">
-        <SideNavigation/>
+        <SideNavigation />
         <div className="pagecontent">
           <h1>
-              {headline}
+            {headline}
           </h1>
-            <p>
-              {panels[textIndex]}
-            </p>
-          <button onClick={this.previousText} className="BackButton">
-            <svg width="21px" height="36px" viewBox="0 0 21 36" version="1.1">
-                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                    <g id="Enter-your-name_v2" transform="translate(-787.000000, -903.000000)" stroke="#FFFFFF" stroke-width="3">
-                        <g id="next_btn" transform="translate(767.000000, 888.000000)">
-                            <polyline id="Path" transform="translate(31.500000, 33.000000) scale(-1, 1) rotate(-270.000000) translate(-31.500000, -33.000000) " points="15.5 41.5 31.5 24.5 31.5 24.5 47.5 41.5"></polyline>
-                        </g>
-                    </g>
-                </g>
-            </svg>
-          </button>
-          <button onClick={this.nextText} className="ForthButton">
-            <svg width="21px" height="35px" viewBox="0 0 21 35" version="1.1">
-                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                    <g id="Enter-your-name_v2" transform="translate(-876.000000, -904.000000)" stroke="#FFFFFF" stroke-width="3">
-                        <g id="next_btn" transform="translate(851.000000, 888.000000)">
-                            <polyline id="Path" transform="translate(35.000000, 33.500000) rotate(-270.000000) translate(-35.000000, -33.500000) " points="19 42 35 25 35 25 51 42"></polyline>
-                        </g>
-                    </g>
-                </g>
-            </svg>
-          </button>
+          <Buttons panels={panels} />
           <p></p>
           <form onSubmit={this.handleSubmit}>
             <label>
               Enter your name:
               <input type="text" value={this.state.username} onChange={this.handleChange} />
             </label>
-            <input type="submit" value="Let's go" onClick={() => this.redirectToNextPage()}/>
+            <input type="submit" value="Let's go" onClick={() => this.redirectToNextPage()} />
           </form>
         </div>
       </div>
--- src/pages/Storypage.jsx
+++ src/pages/Storypage.jsx
@@ -4,95 +4,55 @@ import '../styles/style.css';
 import data from './Story.json';
 import defaultImg from './index.png';
 import { withRouter } from 'react-router';
+import Buttons from '../components/Buttons';
 
 class Storypage extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-        username: '',
-        textIndex: 0,
-        panels: [],
-        headline: ''
-      };
-      this.nextText = this.nextText.bind(this);
-      this.previousText = this.previousText.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      textIndex: 0,
+      panels: [],
+      headline: ''
+    };
+  }
 
-    componentDidMount(){
-        const dataString = JSON.stringify(data);
-        let jsonData = JSON.parse(dataString);
-        console.log(jsonData);
-        this.setState({
-            panels: jsonData[0].panels,
-            headline: jsonData[0].headline
-        })
-    }
+  componentDidMount() {
+    const dataString = JSON.stringify(data);
+    let jsonData = JSON.parse(dataString);
+    console.log(jsonData);
+    this.setState({
+      panels: jsonData[0].panels,
+      headline: jsonData[0].headline
+    })
+  }
 
-     nextText() {
-        const {panels, textIndex} = this.state
-        const theSize = panels.length-1;
-        if(textIndex >= 0 && textIndex < theSize){
-        this.state.textIndex++;
-        this.setState({textIndex: this.state.textIndex});
-        }
-    }
-
-      previousText() {
-        const theSize = this.state.panels.length-1;
-        if(this.state.textIndex > 0 && this.state.textIndex <= theSize){
-            this.state.textIndex--;
-            this.setState({textIndex: this.state.textIndex});
-        }
-      }
-
-      redirectPage(pageName){
-        const {history} = this.props;
-        history.push("/" + pageName);
-    }
+  redirectPage(pageName) {
+    const { history } = this.props;
+    history.push("/" + pageName);
+  }
 
   render() {
-    const {headline, panels} = this.state;
+    const { headline, panels } = this.state;
     return (
-     <div className="Storypage">
-      <SideNavigation/>
-      <div className="pagecontent">
-        <div className="info-section">
+      <div className="Storypage">
+        <SideNavigation />
+        <div className="pagecontent">
+          <div className="info-section">
             <img src={defaultImg} className="defaultImg" alt="logo" />
             <p>
-            {panels[this.state.textIndex]}
+              <Buttons panels={panels} />
             </p>
-       </div>
-        <button onClick={this.previousText} className="BackButton">
-            <svg width="21px" height="36px" viewBox="0 0 21 36" version="1.1">
-                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                    <g id="Enter-your-name_v2" transform="translate(-787.000000, -903.000000)" stroke="#FFFFFF" stroke-width="3">
-                        <g id="next_btn" transform="translate(767.000000, 888.000000)">
-                            <polyline id="Path" transform="translate(31.500000, 33.000000) scale(-1, 1) rotate(-270.000000) translate(-31.500000, -33.000000) " points="15.5 41.5 31.5 24.5 31.5 24.5 47.5 41.5"></polyline>
-                        </g>
-                    </g>
-                </g>
-            </svg>
-         </button>
-        <button onClick={this.nextText} className="ForthButton">
-            <svg width="21px" height="35px" viewBox="0 0 21 35" version="1.1">
-              <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                  <g id="Enter-your-name_v2" transform="translate(-876.000000, -904.000000)" stroke="#FFFFFF" stroke-width="3">
-                      <g id="next_btn" transform="translate(851.000000, 888.000000)">
-                          <polyline id="Path" transform="translate(35.000000, 33.500000) rotate(-270.000000) translate(-35.000000, -33.500000) " points="19 42 35 25 35 25 51 42"></polyline>
-                      </g>
-                  </g>
-              </g>
-            </svg>
-       </button>
-        <p></p>
-        <button onClick={() => this.redirectPage("")}>
+          </div>
+          <p></p>
+          <button onClick={() => this.redirectPage("")}>
             go back
         </button>
-        <button onClick={() => this.redirectPage("decision")}>
+          <button onClick={() => this.redirectPage("decision")}>
             go forward
         </button>
+        </div>
       </div>
-    </div>
     );
   }
 }
