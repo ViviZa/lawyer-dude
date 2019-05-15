ff9363a13206c3c0f458748a6f3c22f903408bf7
Unknown <s0567742@htw-berlin.de>
2019-05-15 21:56:31 +0200

    Add basic Startpage style and lawyer SVG

--- src/pages/Startpage.jsx
+++ src/pages/Startpage.jsx
@@ -3,7 +3,8 @@ import SideNavigation from '../components/SideNavigation';
 import '../styles/style.css';
 import { withRouter } from 'react-router';
 import data from '../data.json';
-import ForthButton from '../components/ForthButton';
+import StartButton from '../components/StartButton';
+import { ReactComponent as LDhappy } from '../images/Lawyerdude-head-happy.svg';
 
 class Startpage extends Component {
   constructor(props) {
@@ -47,12 +48,12 @@ class Startpage extends Component {
 
     return (
       <div className="Startpage">
-        <SideNavigation />
-        <div className="pagecontent">
+        <div className="startpagecontent">
           <h1>{headline}</h1>
-          <h3>an e-learning unit on using pictures from the internet without going to jail</h3>
-          <ForthButton nextText={this.redirectToNextPage} />
+          <h3>An interactive eLearning unit on how to correctly use pictures from the internet without going to jail.</h3>
+          <StartButton nextText={this.redirectToNextPage} />
           <p></p>
+          <LDhappy className="startlawyer"/>  
         </div>
       </div>
     );
