05316853b7ea546e31bebbb0382a2d3c9385556a
vivian.zaddach@gmail.com <youwillnevergetthis10>
2019-06-23 18:26:48 +0200

    add svg to go back button in jail

--- src/pages/Jailpage.jsx
+++ src/pages/Jailpage.jsx
@@ -79,7 +79,18 @@ class Jailpage extends Component {
     return (
       <div className="Jailpage">
         <div className="jailpagecontent">
-        <button onClick={() => this.props.history.goBack()}> ← GO BACK AND TRY AGAIN</button>
+          <button onClick={() => this.props.history.goBack()}> 
+            <svg width="21px" height="36px" viewBox="0 0 21 36" version="1.1">
+                <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                    <g id="Enter-your-name_v2" transform="translate(-787.000000, -903.000000)" stroke="#FFFFFF" strokeWidth="3">
+                        <g id="next_btn" transform="translate(767.000000, 888.000000)">
+                            <polyline id="Path" transform="translate(31.500000, 33.000000) scale(-1, 1) rotate(-270.000000) translate(-31.500000, -33.000000) " points="15.5 41.5 31.5 24.5 31.5 24.5 47.5 41.5"></polyline>
+                        </g>
+                    </g>
+                </g>
+            </svg> 
+            GO BACK AND TRY AGAIN
+          </button>
           <div className="jail-speech">
             <p className="speechbubbletext-jail">
               <div dangerouslySetInnerHTML={{ __html: panels[textIndex]}}/>
