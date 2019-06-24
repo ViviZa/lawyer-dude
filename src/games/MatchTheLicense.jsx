0cc19c8dd8b470831cece1fd155c555987a25cb4
Unknown <simon.binder.94@gmail.com>
2019-06-24 21:41:40 +0200

    showing number of correct answers

--- src/games/MatchTheLicense.jsx
+++ src/games/MatchTheLicense.jsx
@@ -6,6 +6,7 @@ import BackButtonInactive from "../components/BackButtonInactive";
 import ForthButton from "../components/ForthButton";
 import Select from "react-select";
 import update from "immutability-helper";
+import { ReactComponent as LDHeadHappy } from "../images/Lawyerdude-head-happy.svg";
 
 import matchTheLicenseData from "./MatchTheLicenseData.json";
 
@@ -27,6 +28,7 @@ class MatchTheLicense extends Component {
       nextPage: "",
       count: 0,
       questions: [],
+      intro: true,
       selectedOption: [
         {
           option: [],
@@ -64,6 +66,7 @@ class MatchTheLicense extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.validate = this.validate.bind(this);
     this.incCount = this.incCount.bind(this);
+    this.setIntro = this.setIntro.bind(this);
   }
 
   componentDidMount() {
@@ -95,7 +98,7 @@ class MatchTheLicense extends Component {
   }
 
   handleChange(index, checkedOption) {
-    const {count} = this.state;
+    const { count } = this.state;
     this.setState({
       selectedOption: update(this.state.selectedOption, {
         [index + count]: { option: { $set: checkedOption } }
@@ -103,28 +106,48 @@ class MatchTheLicense extends Component {
     });
   }
 
-  incCount(){
-    this.setState({count: 3});
+  incCount() {
+    this.setState({ count: 3 });
   }
+
+  setIntro() {
+    this.setState({ intro: false });
+  }
+
 
   validate() {
     const { selectedOption, questions, count } = this.state;
     let newState = selectedOption;
     for (let i = 0; i < 3; i++) {
+      let correctSelectedSolutions = [];
       const selectedOptionValues = selectedOption[i + count].option.map(
         option => option.value
       );
+      questions[i + count].validOptions.forEach(option =>
+        selectedOptionValues.includes(option)
+          ? correctSelectedSolutions.push(option)
+          : null
+      );
+
       if (
-        questions[i + count].validOptions.length === selectedOptionValues.length &&
-        questions[i + count].validOptions.every(
-          (value, index) => value === selectedOptionValues[index]
-        )
+        questions[i + count].validOptions.length ===
+        correctSelectedSolutions.length
       ) {
-        newState[i + count].solutionText = "Correct Solution!";
+        newState[i + count].solutionText =
+          "Congratulations! You got everything right!";
+        newState[i + count].errorText = "";
+      } else if (correctSelectedSolutions.length === 0) {
+        newState[i + count].errorText =
+          `Not quite right, you didn't get any correct solutions. The correct answers would have been ` +
+          questions[i + count].validOptions.map(option => option);
+        newState[i + count].solutionText = "";
       } else {
         newState[i + count].errorText =
-          "Wrong, correct solution would have been: " +
+          `Almost right, you got ${correctSelectedSolutions.length} of ${
+            questions[i + count].validOptions.length
+          } correct solutions. The correct answers would have been ` +
           questions[i + count].validOptions.map(option => option);
+        newState[i + count].solutionText = "";
       }
     }
     this.setState({ selectedOption: newState });
@@ -132,38 +155,66 @@ class MatchTheLicense extends Component {
 
   render() {
     const { ID } = this.props.location.state;
-    const { selectedOption, questions, count } = this.state;
+    const { selectedOption, questions, count, intro } = this.state;
 
     return (
       <div className="MatchTheLicense">
         <SideNavigation ID={ID} />
         <div className="pagecontent">
           <h1>Match the License</h1>
-          <div>
-            {Array.from(Array(3), (e, i) => {
-              return (
-                <div>
-                  <div>{questions.length > 0 && questions[i + count].text}</div>
-                  <div className="errorMessage">
-                    {selectedOption[i + count].errorText}
+          {intro ? (
+            <div>
+              <div className="speech">
+                <p className="speechbubbletext">
+                  You have experienced that the filter options search engines
+                  offer are not equal to the CC licenses one is actually looking
+                  for. Therefore, it is more important to focus on your
+                  intention when using a picture from the internet. To get an
+                  idea of the right license for your use case we prepared some
+                  examples. Now it’s your turn to find the right license! It is
+                  possible to choose multiple licenses.
+                </p>
+              </div>
+              <div className="speechlawyer-container">
+                <LDHeadHappy className="speechlawyer-happy" />
+              </div>
+            </div>
+          ) : (
+            <div>
+              {Array.from(Array(3), (e, i) => {
+                return (
+                  <div>
+                    <div>
+                      {questions.length > 0 && questions[i + count].text}
+                    </div>
+                    <Select
+                      value={selectedOption[i + count].option}
+                      onChange={ev => this.handleChange(i, ev)}
+                      options={options}
+                      isMulti
+                      className="basic-multi-select"
+                      classNamePrefix="select"
+                      isSearchable={false}
+                    />
+                    <div className="errorMessage">
+                      {selectedOption[i + count].errorText}
+                    </div>
+                    <div className="successMessage">
+                      {selectedOption[i + count].solutionText}
+                    </div>
                   </div>
-                  <div>{selectedOption[i + count].solutionText}</div>
-                  <Select
-                    value={selectedOption[i + count].option}
-                    onChange={ev => this.handleChange(i, ev)}
-                    options={options}
-                    isMulti
-                    className="basic-multi-select"
-                    classNamePrefix="select"
-                    isSearchable={false}
-                  />
-                </div>
-              );
-            })}
-          </div>
-          <button onClick={() => this.validate()}>Check</button>
+                );
+              })}
+              <button onClick={() => this.validate()}>Check</button>
+            </div>
+          )}
         </div>
-        {count !== 3 ? (
+        {intro ? (
+          <div className="buttoncontainer">
+            <BackButtonInactive />
+            <ForthButton nextText={this.setIntro} />
+          </div>
+        ) : count !== 3 ? (
           <div className="buttoncontainer">
             <BackButtonInactive />
             <ForthButton nextText={this.incCount} />
