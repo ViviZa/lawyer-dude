f15afad09251aa400906ece3f4375c7be528bcb2
vivian.zaddach@gmail.com <youwillnevergetthis10>
2019-07-01 09:22:51 +0200

    add jailgame1

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import WelcomePage from './pages/WelcomePage';
 import FindTheLicence from './games/FindTheLicence';
 import Imprint from './pages/Imprint';
 import Overview from './pages/Overview';
+import JailGame1 from './games/JailGame1';
 
 const history = createBrowserHistory()
 library.add(faBars);
@@ -73,6 +74,8 @@ class App extends Component {
           <Route exact path="/findthelicence2" component={() => <FindTheLicence addingPages={this.addingPages}/>}/>
           <Route exact path="/imprint" component={() => <Imprint addingPages={this.addingPages}/>}/>
           <Route exact path="/overview" component={() => <Overview addingPages={this.addingPages}/>}/>
+          <Route exact path="/jailgame1" component={() => <JailGame1 addingPages={this.addingPages}/>}/>
+
       </Router>
     );
   }
