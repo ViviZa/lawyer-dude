5a56d71ed3ca7398fe9e65ddcbce6e17af83888f
Veronika <veronika.babic@web.de>
2019-07-02 14:55:34 +0200

    changes resource text design

--- src/components/ResourcePanel.jsx
+++ src/components/ResourcePanel.jsx
@@ -6,13 +6,16 @@ const ResourcePanel = props => {
 
   return (
     <div className="container">
-      <div row>
-      <div className="speechbubbletext col resourceContainer" >
-        <div dangerouslySetInnerHTML={{ __html: text }} />
+      <div className="row">
+        <div className="resourceContainer">
+      <div className= "resourcetext col">
+        <div dangerouslySetInnerHTML={{ __html: text }}/>
           {images && images.map((image, index) => (
             <img className="resource-image" key={index} src={image} alt="" />
             ))}
-      </div>  
+            <div className="spaceCol">&nbsp;</div>
+      </div>
+      </div>
         {/* <LDFull className="fulllawyer"/> */}
     </div>
     </div>
