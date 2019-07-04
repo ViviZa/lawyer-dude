6f7a1144a521d36ca36d218141932d8ca3745586
Mario Schuetz <mayo2@gmx.de>
2019-07-04 19:01:55 +0200

    Extract resources from data json

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
+import { Link } from "react-router-dom";
 import data from '../data.json';
-import { ReactComponent as LDhappy } from '../images/Lawyerdude-head-happy.svg';
 import SettingsButton from '../components/SettingsButton.jsx';
 
 class Resources extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
       headline: '',
+      resources: [],
+      glossar: '',
+      content: '',
     };
 
     this.redirectToNextPage = this.redirectToNextPage.bind(this);
@@ -19,26 +21,48 @@ class Resources extends Component {
     const { ID } = this.props.location.state;
     const dataString = JSON.stringify(data);
     let jsonData = JSON.parse(dataString);
+    // this.resources(jsonData);
     const filteredJSON = jsonData.filter(values => values.id === ID);
     localStorage.removeItem('visitedPages');
     localStorage.setItem('visitedPages', JSON.stringify(filteredJSON));
+
+    let resources = [];
+    jsonData.forEach(obj => {
+      if(obj.panels !== undefined){
+        obj.panels.forEach(panel => {
+          if(panel.type === 'resource') resources.push(obj);
+        });
+      }
+    });
+    let glossar = '';
+    let content = '';
+    resources.forEach ( resource => {
+      let headline = resource.headline;
+      glossar += "<div classname='resourceGlossar'><Link to='#" + headline + "'>" + headline + "</Link></div>"  
+      // let text = resource.text;
+      // content += "<div className='contentHeadline'><h2 id=" + headline + ">" + headline + "</h2></div><div className='contentText'>" + text + "</div>";
+    });
+    
     this.setState({
       headline: filteredJSON[0].headline,
+      glossar: glossar,
+      content: content,
     })
   }
-
+  
   redirectToNextPage(index) {
     const { history } = this.props;
     const { nextPageIDs, nextPages } = this.state;
-
+    
     history.push({
       pathname: nextPages[index],
       state: { ID: nextPageIDs[index] },
     });
   }
-
+  
   render() {
-    const { headline } = this.state;
+    const { headline, glossar, content } = this.state;
+    console.log(glossar);
     return (
       <div className="Resources">
         <SettingsButton goBack={() => this.props.history.goBack()} />
@@ -48,7 +72,7 @@ class Resources extends Component {
         <div className="pagecontent   ">
           <h1 className="headline">{headline}</h1>
           <div className="container">
-              <LDhappy className="boxlawyer" />
+          <div dangerouslySetInnerHTML={{ __html: glossar }} />
           </div>
         </div>
       </div>
